dcb433e0dc605be1909bc65eab362242213af444
napasto <webeasystudio@gmail.com>
2022-10-04 15:43:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,8 @@
     }).then(
         response => response.json()
     ).then((html) => {
-      // you can put any JS code here
-let my_url = "https://softorg.framer.website/thank";
-window.onload = function (){ window.location.replace(my_url);
-  // body...
-}
+     window.open('https://softorg.framer.website/thank', '_blank');
+
     } ) ;
   } );
 </script>
