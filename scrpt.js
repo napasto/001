0769fd673716dee74deb724291aa370f0250a0a8
napasto <webeasystudio@gmail.com>
2022-10-05 16:12:53 +0300

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,3 @@
+<script type="text/javascript" >var submitted=false </script>
+<iframe name="hiddenConfirm" id="hiddenConfirm" style="display:none;" onload="if(submitted){ 
+	window.location='https://trustworthy-button-156944.framer.app/thnx';}"> </iframe>
